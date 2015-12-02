596bd8b58a717f4811f22ca0c1ac4ea5c5ae9e32
Daniel Rempel <danil.rempel@gmail.com>
2015-12-03 00:27:22 +0200

    parse-xml: fix reference issue; convert spaces to tabs, crlf -> lf

--- ladle.lua
+++ ladle.lua
@@ -11,9 +11,9 @@ xmlp = require("xml.parser")
 
 -- detect operating system
 if os.getenv("WinDir") ~= nil then
-        os = "Windows"
+	os = "Windows"
 else
-        os = "Other OS"  -- !
+	os = "Other OS"  -- !
 end
 
 -- load mime configuration file
@@ -22,126 +22,126 @@ if mconf ~= nil then mconf = mconf:read("*all") end
 
 -- start web server
 function main(arg1) 
-    port = arg1 -- set first argument as port
+	port = arg1 -- set first argument as port
 
-    -- display initial program information
-    print [[Ladle web server v0.1.1
+	-- display initial program information
+	print [[Ladle web server v0.1.1
 Copyright (c) 2008 Samuel Saint-Pettersen]]
 
-    -- if no port is specified, use port 80
-    if port == nil then port = 80 end
+	-- if no port is specified, use port 80
+	if port == nil then port = 80 end
 
-    -- display warning message if configuration missing
-    if mconf == nil then
-        print("\nWarning: MIME config file missing")
-    end 
+	-- display warning message if configuration missing
+	if mconf == nil then
+		print("\nWarning: MIME config file missing")
+	end 
 
-    -- create tcp socket on localhost:$port
-    server = assert(socket.bind("*", port))
+	-- create tcp socket on localhost:$port
+	server = assert(socket.bind("*", port))
 
-    -- display message to web server is running
-    print("\nRunning on localhost:" .. port)
-    waitReceive() -- begin waiting for client requests
+	-- display message to web server is running
+	print("\nRunning on localhost:" .. port)
+	waitReceive() -- begin waiting for client requests
 end
 -- wait for and receive client requests
 function waitReceive()
-    -- loop while waiting for a client request
-    while 1 do
-        -- accept a client request
-        client = server:accept()
-        -- set timeout - 1 minute.
-        client:settimeout(60)
-        -- receive request from client
-        local request, err = client:receive()
-        -- if there's no error, begin serving content or kill server
-        if not err then
-            -- if request is kill (via telnet), stop the server
-            if request == "kill" then
-                client:send("Ladle has stopped\n")
-                print("Stopped")
-                break
-            else
-                -- begin serving content
-                serve(request)
-            end
-        end
-    end
+	-- loop while waiting for a client request
+	while 1 do
+		-- accept a client request
+		client = server:accept()
+		-- set timeout - 1 minute.
+		client:settimeout(60)
+		-- receive request from client
+		local request, err = client:receive()
+		-- if there's no error, begin serving content or kill server
+		if not err then
+			-- if request is kill (via telnet), stop the server
+			if request == "kill" then
+				client:send("Ladle has stopped\n")
+				print("Stopped")
+				break
+			else
+				-- begin serving content
+				serve(request)
+			end
+		end
+	end
 end
 -- serve requested content
 function serve(request)
-    -- resolve requested file from client request
-    local file = string.match(request, "%w+%/?.?%l+")
-    -- if no file mentioned in request, assume root file is index.html.
-    if file == nil then
-        file = "index.html"
-    end
-        
-    -- retrieve mime type for file based on extension
-    local ext = string.match(file, "%[.]%l%l%l%l?")
-    local mime = getMime(ext)
+	-- resolve requested file from client request
+	local file = string.match(request, "%w+%/?.?%l+")
+	-- if no file mentioned in request, assume root file is index.html.
+	if file == nil then
+		file = "index.html"
+	end
+		
+	-- retrieve mime type for file based on extension
+	local ext = string.match(file, "%[.]%l%l%l%l?")
+	local mime = getMime(ext)
 
-    -- reply with a response, which includes relevant mime type
-    if mime ~= nil then
-        client:send("HTTP/1.1 200/OK\r\nServer: Ladle\r\n")
-        client:send("Content-Type:" .. mime .. "\r\n\r\n")
-    else
+	-- reply with a response, which includes relevant mime type
+	if mime ~= nil then
 		client:send("HTTP/1.1 200/OK\r\nServer: Ladle\r\n")
-        client:send("Content-Type: text/html; encoding: utf8\r\n\r\n") 
-    end
+		client:send("Content-Type:" .. mime .. "\r\n\r\n")
+	else
+		client:send("HTTP/1.1 200/OK\r\nServer: Ladle\r\n")
+		client:send("Content-Type: text/html; encoding: utf8\r\n\r\n") 
+	end
 
-    -- determine if file is in binary or ASCII format
-    local binary = isBinary(mime)
+	-- determine if file is in binary or ASCII format
+	local binary = isBinary(mime)
 
-    -- load requested file in browser
-    local served, flags
-    if binary == false then
-        -- if file is ASCII, use just read flag
-        flags = "r"     
-    else
-        -- otherwise file is binary, so also use binary flag (b)
-        -- note: this is for operating systems which read binary
-        -- files differently to plain text such as Windows
-        flags = "rb"
-    end
-    served = io.open("www/" .. file, flags)
-    if served ~= nil then
-        local content = served:read("*all")
-        client:send(content)
-    else
-        -- display not found error
-        err("Not found!")
-    end
+	-- load requested file in browser
+	local served, flags
+	if binary == false then
+		-- if file is ASCII, use just read flag
+		flags = "r"     
+	else
+		-- otherwise file is binary, so also use binary flag (b)
+		-- note: this is for operating systems which read binary
+		-- files differently to plain text such as Windows
+		flags = "rb"
+	end
+	served = io.open("www/" .. file, flags)
+	if served ~= nil then
+		local content = served:read("*all")
+		client:send(content)
+	else
+		-- display not found error
+		err("Not found!")
+	end
 
-    -- done with client, close request
-    client:close()
+	-- done with client, close request
+	client:close()
 end
 -- determine mime type based on file extension
 function getMime(ext)
-    local i = 1
-    local exts = xmlp.ctag(mconf, "file")
-    while i < exts do
-        local v = xmlp.vatt(mconf, "file", "ext", i)
-        if v == ext then
-            return xmlp.vtag(mconf, "mime", i)
-        end
-        i = i + 1
-    end
+	local i = 1
+	local exts = xmlp.ctag(mconf, "file")
+	while i < exts do
+		local v = xmlp.vatt(mconf, "file", "ext", i)
+		if v == ext then
+			return xmlp.vtag(mconf, "mime", i)
+		end
+		i = i + 1
+	end
 end
 -- determine if file is binary - true or false
 function isBinary(mime)
-    local i = 1
-    local types = xmlp.ctag(mconf, "mime")
-    while i < types do
-        local v = xmlp.vtag(mconf, "mime", i)
-        if v == mime then
-            return xmlp.vtag(mconf, "bin", i)
-        end
-        i = i + 1
-    end
+	local i = 1
+	local types = xmlp.ctag(mconf, "mime")
+	while i < types do
+		local v = xmlp.vtag(mconf, "mime", i)
+		if v == mime then
+			return xmlp.vtag(mconf, "bin", i)
+		end
+		i = i + 1
+	end
 end     
 -- display error message and server information
 function err(message)
-    client:send(message)
+	client:send(message)
 	-- ...
 end
 -- invoke program starting point:
--- xml/parser.lua
+++ xml/parser.lua
@@ -17,62 +17,62 @@ parser.val = "[%w%x%s/\\.]+"
 
 -- parse tag
 function parser.vtag(data, tag, occur)
-    if data and tag and occur ~= nil then
-        local tm = {}
-        local i = 1
-        local ftag = "</" .. tag .. ">"
-        local query = val .. ftag
-        for mv in string.gmatch(data, query) do
-            tm[i] = string.gsub(mv, ftag, "")
-            i = i + 1
-        end
-        if tm[occur] ~= nil then
-            return tm[occur]
-        else
-            return "Tag: " .. tag .."[" .. occur .. "] does not exist"
-        end          
-    else
-        return "Usage: r = xmlp.vtag(data as str, tag as str, occur as int)"
-    end
+	if data and tag and occur ~= nil then
+		local tm = {}
+		local i = 1
+		local ftag = "</" .. tag .. ">"
+		local query = parser.val .. ftag
+		for mv in string.gmatch(data, query) do
+			tm[i] = string.gsub(mv, ftag, "")
+			i = i + 1
+		end
+		if tm[occur] ~= nil then
+			return tm[occur]
+		else
+			return "Tag: " .. tag .."[" .. occur .. "] does not exist"
+		end          
+	else
+		return "Usage: r = xmlp.vtag(data as str, tag as str, occur as int)"
+	end
 end
 -- parse attribute
 function parser.vatt(data, tag, att, occur)
-    if data and tag and att and occur ~= nil then
-        local am = {}
-        local i = 1
-        local fatt = "<" .. tag .. "%s" .. att .. "=\"?'?"
-        local query = fatt .. val
-        for mv in string.gmatch(data, query) do
-            am[i] = string.gsub(mv, fatt, "")
-            i = i + 1
-        end
-        if am[occur] ~= nil then
-            return am[occur]
-        else
-            return "Attribute: " .. att .. " @ " .. tag .. "[" .. occur .. "] does not exist"
-        end
-    else
-        return [[Usage: r = xmlp.vatt(data as str, tag as str, attr as str
+	if data and tag and att and occur ~= nil then
+		local am = {}
+		local i = 1
+		local fatt = "<" .. tag .. "%s" .. att .. "=\"?'?"
+		local query = fatt .. parser.val
+		for mv in string.gmatch(data, query) do
+			am[i] = string.gsub(mv, fatt, "")
+			i = i + 1
+		end
+		if am[occur] ~= nil then
+			return am[occur]
+		else
+			return "Attribute: " .. att .. " @ " .. tag .. "[" .. occur .. "] does not exist"
+		end
+	else
+		return [[Usage: r = xmlp.vatt(data as str, tag as str, attr as str
 , occur as int)]] 
-    end
+	end
 end
 -- count tags
 function parser.ctag(data, tag)
-    if data ~= nil then
-        local pattern
-        local count = 0
-        if tag ~= nil then
-            pattern = "<" .. tag .. "[%s%w%x='\".]*/?>"
-        else
-            pattern = "<[%w%x%s-\"'=.]+/?>"
-        end
-        for mt in string.gmatch(data, pattern) do
-            count = count + 1
-        end
-        return count
-    else
-        return "Usage: r = xmlp.ctag(data as str [, tag as str])"
-    end
+	if data ~= nil then
+		local pattern
+		local count = 0
+		if tag ~= nil then
+			pattern = "<" .. tag .. "[%s%w%x='\".]*/?>"
+		else
+			pattern = "<[%w%x%s-\"'=.]+/?>"
+		end
+		for mt in string.gmatch(data, pattern) do
+			count = count + 1
+		end
+		return count
+	else
+		return "Usage: r = xmlp.ctag(data as str [, tag as str])"
+	end
 end     
 
 return parser
